846e2c873fdf2d4591f360b3053424f38c597d6c
SeaHawks <tanvirrezaanik@gmail.com>
2022-08-22 18:05:58 +0600

    Icons on stat cards dynamic

--- resources/js/components/requestStatus/index.js
+++ resources/js/components/requestStatus/index.js
@@ -1,21 +1,36 @@
-import React, {useState, useEffect} from "react";
-import {FaHourglassHalf} from "react-icons/fa";
+import React, {useEffect, useState} from "react";
 import '../../style/requestStatus.scss'
 import {BeatLoader} from "react-spinners";
 import {useLocation} from "react-router-dom";
+import {AiOutlineClose} from "react-icons/ai";
+import {MdOutlineDone} from "react-icons/md";
+import {IoIosHourglass} from "react-icons/io";
 
 const RequestStatus = ({count, requestStatus, iconBg}) => {
-    const location=useLocation()
-    const path=location.pathname
+    const location = useLocation()
+    const path = location.pathname
+    const [size, setSize] = useState('')
+
+    useEffect(() => {
+        setSize(path.includes('dashboard') ? '40px' : '22px')
+    }, []);
+
 
     return (
-            <div className='requestStatus'>
+        <div className='requestStatus'>
             <div className='flexStatus'>
-                <div className='wrapIcon' style={{backgroundColor:iconBg}}>
-                    <FaHourglassHalf size={path.includes('dashboard') ?'40px':'22px'} color='white'/>
+                <div className='wrapIcon' style={{backgroundColor: iconBg}}>
+                    {
+                        requestStatus === 'Pending' ?
+                            <IoIosHourglass size={size} color='white'/>
+                            : requestStatus === 'Rejected' ?
+                                <AiOutlineClose size={size} color='white'/>
+                                :
+                                <MdOutlineDone size={size} color='white'/>
+                    }
                 </div>
-                <div className={!path.includes('dashboard') ?'status':'statusInfo'}>
-                    <h1>{count || count===0? count : <BeatLoader size={10} color={'#000000'}/>}</h1>
+                <div className={!path.includes('dashboard') ? 'status' : 'statusInfo'}>
+                    <h1>{count || count === 0 ? count : <BeatLoader size={10} color={'#000000'}/>}</h1>
                     <h2>{requestStatus} Requests</h2>
                 </div>
             </div>
