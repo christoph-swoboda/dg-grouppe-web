109000ca640aa5ae96e17456a5c3345be7eb58f1
Anikreza <tanvirrezaanik@gmail.com>
2025-08-26 09:45:30 +0600

    temp fix on commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,10 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
 
-    protected $commands = [
-        Commands\SendPushNotification::class,
-    ];
-
     /**
      * Define the application's command schedule.
      *
